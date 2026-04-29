b6bbda2e411b5d986c1e9aec577160c5e0060e8f
Todd Thomas <noodlepopllc@gmail.com>
2026-04-29 10:45:43 -0400

    fixed bug in config

--- lib/config.py
+++ lib/config.py
@@ -12,7 +12,7 @@ def load_config():
         "DIFFSYNTH_DOWNLOAD_SOURCE": "huggingface", 
         "DIFFSYNTH_SKIP_DOWNLOAD": "False", 
         "BITSNBYTES":"True","BATCH":"False", 
-        "BACKEND" "transformers",
+        "BACKEND": "transformers",
         "OLLAMA_URL", "http://localhost:11434"
     }
     if os.path.exists(CONFIG_FILE):
