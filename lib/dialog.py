e5f51038d71abc8ccdf244505e8a9205c090db61
Todd Thomas <noodlepopllc@gmail.com>
2026-06-30 23:30:26 -0400

    Updated for short sentences

--- lib/dialog.py
+++ lib/dialog.py
@@ -40,10 +40,14 @@ def create_audio_and_free_vram(
     - optional Whisper semantic verification
     """
 
+
     # Optional reference transcription
     if ref_audio and not ref_text:
         segs = transcribe(ref_audio)
         ref_text = " ".join(segs)
+
+    if len(ref_text.split(' ')) < 5:
+        ref_text = 'ERROR!!! Not enough input text!'
 
     start_silence_ms = 300
     end_silence_ms = 500
