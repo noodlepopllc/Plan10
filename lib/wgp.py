afce86b1b0ef96bc0753e59a0e2f55fe5dd9288c
Todd Thomas <noodlepopllc@gmail.com>
2026-07-04 08:11:05 -0400

    Increased syllable threshold for longer length videos

--- lib/wgp.py
+++ lib/wgp.py
@@ -57,10 +57,10 @@ async def i2v(prompt='', media='', output='output.mp4',
         args['prompt'] = final_prompt
         args['image_prompt_type'] =  'S'
         args['image_start'] = media
-        args['guidance_scale'] = 3.5
-        args['sample_solver'] = "euler"
-        args['negative_prompt'] = negative_prompt
-        args['num_inference_steps'] = 30
+        #args['guidance_scale'] = 2.0
+        #args['sample_solver'] = "euler"
+        #args['negative_prompt'] = negative_prompt
+        #args['num_inference_steps'] = 30
         #args['prompt_enhancer'] = 'TI'
         #args['audio_source'] = None
         #args['audio_prompt_type'] = 'A'
