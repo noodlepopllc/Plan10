590b31f2e3daa99a134e48b460934b88dedc7e79
Todd Thomas <noodlepopllc@gmail.com>
2026-07-04 19:19:57 -0400

    Updated

--- Planning/renderer/renderer.py
+++ Planning/renderer/renderer.py
@@ -398,15 +398,18 @@ def render_beats_dialog(assets, actions, mappings, T):
             final_alias = f"BEAT_{beat['beat']}_{normalize(speaker)}_DIALOG_VIDEO_{s_idx:02d}"
             T.dialog_final(final_alias, base_alias_closeup, f"{speaker_alias}_VOICE", line, '')
             final_alias = f"BEAT_{beat['beat']}_{normalize(speaker)}_DIALOG_VIDEO_MEDIUM_{s_idx:02d}"
+            loc_alias = base_alias_medium
             if os.environ.get('LTX','False') == 'True':
                 all_names = build_identity_map(assets, beat)
                 beat_chars = get_beat_characters(beat, all_names)
                 names = {c: all_names[c] for c in beat_chars}
                 arc = beat.get('arc', '')
                 final_arc = resolve_character_mentions(arc, names)
+                if len(beat_chars) > 1:
+                    loc_alias = base_alias_ots
             else:
                 final_arc = ''
-            T.dialog_final(final_alias, base_alias_medium, f"{speaker_alias}_VOICE", line, final_arc)
+            T.dialog_final(final_alias, loc_alias, f"{speaker_alias}_VOICE", line, final_arc)
             s_idx += 1
 
 
