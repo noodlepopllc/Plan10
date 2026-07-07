14436e8e5709364e2426e265f352d3f5ef53f07f
Todd Thomas <noodlepopllc@gmail.com>
2026-07-07 11:10:30 -0400

    Updated alice

--- Planning/builders/storywriter.py
+++ Planning/builders/storywriter.py
@@ -24,29 +24,6 @@ Initial Situation:
 Lisa is texting on her bed. Maria enters the room.
 """
 
-SEED = """
-Characters:
-- Alora: princess, short tattered low cut tight fitting brown dress, blonde hair, prisoner of Quin
-- Quin: sorceress, believes rightful queen, aunt of Alora, regal red dress, black hair in a bun with a crown
-
-Location: medieval style dungeon in a fantasy world of high magic
-
-Motivations:
-- Alora wants to be free.
-- Quin wants her sister Alora's mother to step down and make her the queen.
-
-Spark:
-Alora has been captured by Quin and is now being kept in a dungeon.
-
-Story Goal:
-Quin wants to convince Alora she isn't evil, her crown was stolen and her mother is secretly a witch.
-Quin only learned the dark arts in order to combat her sister.
-Alora wants to be freed and her aunt Quin to be punished.
-
-Initial Situation:
-Alora is standing in front of Quin beneath an abandoned castle in a dank dungeon.
-"""
-
 SYSTEM = """
 Using the structured seed below, write a coherent cinematic scene of 8–12 beats.
 Do not introduce new characters or objects.
