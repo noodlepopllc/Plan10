97b9cb76194ab51f09591848ae967f60ea09f4ca
Todd Thomas <noodlepopllc@gmail.com>
2026-07-14 09:22:23 -0400

    Added two character version of improv

--- lib/improv.py
+++ lib/improv.py
@@ -141,7 +141,6 @@ def feedback_loop(initial_media, story_context, output_dir="feedback_output", ma
         detailer.enhance(
             image=last_frame,
             output_path=str(enhanced_frame_path),
-            scale=0.4,
             seed=42 + beat
         )
 
