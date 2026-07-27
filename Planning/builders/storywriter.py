959cf277714252e4d195a1ababcffd18abef3386
Todd Thomas <noodlepopllc@gmail.com>
2026-07-26 23:39:27 -0400

    increased token budget for story

--- Planning/builders/storywriter.py
+++ Planning/builders/storywriter.py
@@ -325,7 +325,7 @@ def run_prompt(prompt, system, pth):
             media="",
             prompt=prompt,
             system=system,
-            max_tokens=8192)['analysis']
+            max_tokens=32000)['analysis']
         with open(pth, 'w') as out_f:
             out_f.write(result)
         print(f'Wrote {pth}')
