9223d9d3c853c4ada339daed6d77c59f91be400f
Todd Thomas <noodlepopllc@gmail.com>
2026-08-04 17:37:34 -0400

    tweaked wan2.2

--- plan10/lib/mmh3.py
+++ plan10/lib/mmh3.py
@@ -259,7 +259,7 @@ def GenerateTalkingVideo(
     width = int(width)
     height = int(height)
     seed = int(seed)
-    duration_sec = int(estimate_duration(text))
+    duration_sec = 5 # int(estimate_duration(text))
     fps = 24
 
     if seed == -1:
