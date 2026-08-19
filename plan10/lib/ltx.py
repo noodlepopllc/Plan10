b039c4fa5c74cea4107393dc8f12b5e3f75a488d
Todd Thomas <noodlepopllc@gmail.com>
2026-08-19 16:35:37 -0400

    Updated to create transcript for audio

--- plan10/lib/ltx.py
+++ plan10/lib/ltx.py
@@ -15,6 +15,7 @@ from plan10.lib.util import video_to_img
 from plan10.lib.image_analysis import AnalyzeImage, EnhancePrompt
 from plan10.lib.image_gen import add_metadata_char
 from plan10.lib.dialog import CloneVoice
+import numpy as np
 import librosa
 import random
 
