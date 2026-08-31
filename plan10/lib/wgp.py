db6adc34dfde7e5f443abe91748a723d098b3be6
Todd Thomas <noodlepopllc@gmail.com>
2026-08-31 11:57:56 -0400

    Created option to choose full pipeline vs distilled

--- plan10/lib/wgp.py
+++ plan10/lib/wgp.py
@@ -22,6 +22,7 @@ ANIME = "_anime" if os.environ.get("ANIME","False") != "False" else ""
 ARC = os.environ.get("LTX","False") == "ARC"
 MMH3 = os.environ.get('MMH3','False') != 'False'
 VERBOSE = os.environ.get('VERBOSE','False') != 'False'
+DISTILLED = 'DISTILLED' in  os.environ.get('LTX','False')
 
 MAX_DURATION = 5 if MMH3 else 10
 
@@ -37,8 +38,7 @@ enhance_path = f'./system/ltx_enhancer_minimal{ANIME}.txt' if BRIEF else f'./sys
 if MMH3:
     enhance_path = './system/mmh3_enhancer.txt'
 
-#tool = "ltx2_22B_1_1"
-tool = "ltx2_22B_distilled_1_1"
+tool = "ltx2_22B_distilled_1_1" if DISTILLED else "ltx2_22B_1_1"
 
 async def i2v_ltx(prompt='', media='', end='', output='output.mp4', 
                   duration_sec=5, width=WIDTH, height=HEIGHT, seed=-1):
@@ -268,7 +268,7 @@ async def s2v_ltx(prompt='', media='', end_image='', audio='', text='', output='
                   duration_sec=5, width=WIDTH, height=HEIGHT, seed=-1):
     async with Client("http://localhost:7866/mcp") as client:
 
-        model = "ltx2_22B_distilled_1_1"
+        model = tool
 
         r = await client.call_tool("wangp_get_default_settings", {"model_type":model})
         results = json.dumps(r.data, indent=4)
