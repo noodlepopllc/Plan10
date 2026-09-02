800155549e067dca9d34718d88768e5d9e37c75c
Todd Thomas <noodlepopllc@gmail.com>
2026-09-02 10:56:47 -0400

    Lowered resolutions of reference images

--- plan10/lib/director_mmh3.py
+++ plan10/lib/director_mmh3.py
@@ -482,7 +482,6 @@ def main():
         
         # --- AUDIO REFERENCES (with voice generation prompts) ---
         # audio | label   | path                    | target  | extra_desc                          | voice_prompt
-        audio | voice_b | audio/voice_sample.wav  | blondie | containing a spoken English vocal layer | female
         audio | voice_r | audio/voice_sample2.wav | red     | containing a spoken English vocal layer | female
         
         # --- SCENE CONTEXT ---
@@ -492,9 +491,8 @@ def main():
         # --- SHOTS ---
         shot | The camera pushes on the inside of the barn. blondie is standing near the door holding a treat. | 2.0
         shot | The dog runs into the barn and jumps up to grab the treat from blondie.
-        shot | red walks over to blondie and speaks, [English] You spoil him. | 2
+        shot | red walks over to blondie, red speaks, [English] You spoil him. | 2
         shot | Closeup shot of blondie | 1
-        shot | blondie speaks to dog [English] You are such a good boy! | 2.0
         """
 
     # Build and execute
