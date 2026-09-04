e06e689ebb3014d590d4f7626d15d278e9847eea
Todd Thomas <noodlepopllc@gmail.com>
2026-09-04 12:28:39 -0400

    Lip sync additions for ID Lora

--- plan10/lib/wgp.py
+++ plan10/lib/wgp.py
@@ -505,7 +505,7 @@ def GenerateTalkingVideo(
     width = int(width)
     height = int(height)
     seed = int(seed)
-    estimated =  int(estimate_duration(text))
+    estimated =  int(estimate_duration(text)) + 1
     print(f"ESTIMATED DURATION: {estimated} s")
     if max_duration == -1:
         duration_sec = estimated
