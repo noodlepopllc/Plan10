92fc92eb31e6b3ec91802ee05300e6baae3ca7f1
Todd Thomas <noodlepopllc@gmail.com>
2026-09-07 11:10:31 -0400

    Updated

--- plan10/lib/brain.py
+++ plan10/lib/brain.py
@@ -90,6 +90,7 @@ def prepare_context_for_llm(raw_context: dict) -> dict:
             asset_data["path"] = p.name
             
     del llm_context['history']
+    print(llm_context)
     return llm_context
 
 # =============================================================================
