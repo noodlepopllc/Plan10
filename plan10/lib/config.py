1231a6a9244afdb550e66bb3fc27f102f9d947be
Todd Thomas <noodlepopllc@gmail.com>
2026-09-08 13:20:48 -0400

    Updated config to include diffysnth path

--- plan10/lib/config.py
+++ plan10/lib/config.py
@@ -97,7 +97,7 @@ def setconfig(mmh3=False, ltx2=False, hires=False, sdres=False, portrait=False, 
         tmp_cfg['LTX'] = 'False'
         high_resolution = ("1344", "768") 
         standard_resolution = ("864", "480")
-        minimal_resolution = ("512", "512") # will not look good but is good for testing
+        minimal_resolution = ("768", "512") # will not look good but is good for testing
     elif ltx2:
         tmp_cfg["LTX"] = "DISTILLED"  # Fixed: missing = operator
         tmp_cfg['MMH3'] = 'False'
