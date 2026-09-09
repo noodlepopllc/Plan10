a92bde950a0f87d281c320247414991cd76d2beb
Todd Thomas <noodlepopllc@gmail.com>
2026-09-09 13:04:10 -0400

    Updated klein

--- plan10/emergent/video_creator.py
+++ plan10/emergent/video_creator.py
@@ -10,7 +10,7 @@ from plan10.emergent.character import CharacterProfile
 from plan10.emergent.pipeline import Pipeline
 from plan10.lib.scene_analyzer import analyze_scene
 from plan10.lib.util import extract_frame
-from PIL import image
+from PIL import Image
 
 from plan10.lib.decomposer import decompose_scene
 
