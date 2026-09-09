43d3d681a633f0a27ecdbb842fba3943e0ccb305
Todd Thomas <noodlepopllc@gmail.com>
2026-09-09 18:16:00 -0400

    Updated wangp code for minimax h3 r2v

--- plan10/emergent/video_runner.py
+++ plan10/emergent/video_runner.py
@@ -256,10 +256,7 @@ def main():
         # Resize background to target video resolution (e.g., 768x448)
         if bg and os.path.exists(bg):
             bg_img = Image.open(bg).convert("RGB")
-            if WIDTH > HEIGHT:
-                bg_img = bg_img.resize((WIDTH, HEIGHT), resample_filter)
-            else:
-                bg_img = bg_img.resize(HEIGHT, WIDTH), resample_filter)
+            bg_img = bg_img.resize((768, 448), resample_filter)
             bg_resized = os.path.join(output_dir, "resized_bg.png")
             bg_img.save(bg_resized)
             bg_img.close()
