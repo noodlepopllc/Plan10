ab388ca218baa25aa4c27de894840e439616d6a9
Todd Thomas <noodlepopllc@gmail.com>
2026-09-10 12:35:08 -0400

    fixed typo

--- plan10/lib/image_gen.py
+++ plan10/lib/image_gen.py
@@ -328,7 +328,7 @@ elif os.environ.get("IMAGE_GEN", "KLEIN") == "ZIMAGE":
     ImageGen = ImageGenZImage
 elif os.environ.get("IMAGE_GEN", "KLEIN") == "KREA2":
     ImageGen = ImageGenKrea2
-elif os.environ.get("IMAGE_GET", "KLEIN") == "SENSENOVA":
+elif os.environ.get("IMAGE_GEN", "KLEIN") == "SENSENOVA":
     ImageGen = ImageGenSenseNova
 else:
     ImageGen = ImageGenQwen
