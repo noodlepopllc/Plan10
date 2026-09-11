cdfebcfb7f65251ed978667d84ef8876c469d788
Todd Thomas <noodlepopllc@gmail.com>
2026-09-11 16:56:32 -0400

    Updated to resolve clash in argument naming.

--- plan10/emergent/video_runner.py
+++ plan10/emergent/video_runner.py
@@ -141,7 +141,7 @@ def h3_ref(bg, ff, refs, portraits, prompt, duration=10.0):
             script += f"portrait | portrait_{ndx} | {portraits[portrait_ndx]} | {label} | {portrait_desc}"
             portrait_ndx += 1
         else:
-            port_path = os.path.splitext(ref)[0] '_portrait.png'
+            port_path = os.path.splitext(ref)[0] + '_portrait.png'
             portrait_desc = get_or_analyze(ref, ,
                 'Brief description of characters face and hair, Max 10 words.', 'portrait_desc')
             script += f"portrait | portrait_{ndx} | {port_path} | {label} | {portrait_desc}"
