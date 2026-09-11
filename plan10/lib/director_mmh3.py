7a02c07438e0de3dc3c872fc92777b0a6fcbee89
Todd Thomas <noodlepopllc@gmail.com>
2026-09-11 12:32:16 -0400

    Added portrait

--- plan10/lib/director_mmh3.py
+++ plan10/lib/director_mmh3.py
@@ -663,8 +663,9 @@ def main():
         soundscape  | Ambient wind and soft acoustic guitar music.
         
         # --- SHOTS ---
-        shot | The camera pushes on the inside of the barn. blondie is standing near the door holding a treat. | 2.0
-        shot | The dog runs into the barn and jumps up to grab the treat from blondie. | 2
+        shot | Sound of roosters, as The camera pushes on the inside of the barn. blondie is standing near the door holding a treat and red stands beside her with her arms crossed. | 2.0
+        shot | Sounds of a barking dog, Following shot The dog runs into the barn and jumps up to grab the treat from blondie. | 2
+        shot | Sounds of cows mooing, Camera pushes in on red | 1
         shot | Closeup shot of red, red speaks, [English] You spoil him. | 2
         """
 
