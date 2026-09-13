e61f37cc534a131f6be9bc7566fc32c97fbd995a
Todd Thomas <noodlepopllc@gmail.com>
2026-09-13 14:17:25 -0400

    Added preview.sh

--- plan10/emergent/video_runner.py
+++ plan10/emergent/video_runner.py
@@ -150,7 +150,7 @@ def h3_ref(bg, ff, refs, portraits, prompt, duration=10.0, visual_ids=[]):
         script += f"ff | ff | {ff} | {ff_desc}\n"
     
     # 2. Background - CACHED
-    bg_desc = get_or_analyze(bg, BG_PROMPT, 'description', max_words=100)
+    bg_desc = get_or_analyze(bg, BG_PROMPT, 'Description', max_words=100)
     script += f"bg | bg | {bg} | {bg_desc}\n"
     
     # 3. Generate shots FIRST to know who speaks
@@ -177,15 +177,15 @@ def h3_ref(bg, ff, refs, portraits, prompt, duration=10.0, visual_ids=[]):
     portrait_entries = ''
     for ndx, ref in enumerate(refs, start=1):
         label = f"char{ndx}"
-        char_desc = get_or_analyze(bg, CHAR_PROMPT, 'description', max_words=100)
+        char_desc = get_or_analyze(bg, CHAR_PROMPT, 'Description', max_words=100)
         script += f"char | {label} | {ref} | {char_desc}\n"
 
         if portraits:
-            portrait_desc = get_or_analyze(portraits[ndx-1], FACE_PROMPT, 'description', max_words=100)
+            portrait_desc = get_or_analyze(portraits[ndx-1], FACE_PROMPT, 'Description', max_words=100)
             portrait_entries += f"portrait | portrait_{ndx} | {portraits[ndx-1]} | {label} | {portrait_desc}\n"
         else:
             port_path = os.path.splitext(ref)[0] + '_portrait.png'
-            portrait_desc = get_or_analyze(ref, FACE_PROMPT, 'description', max_words=100)
+            portrait_desc = get_or_analyze(ref, FACE_PROMPT, 'Description', max_words=100)
             portrait_entries += f"portrait | portrait_{ndx} | {port_path} | {label} | A portrait of {label}\n"
         
         # Only generate audio if this character speaks
