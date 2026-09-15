fc2b95f4b70c95985a97b1543da9b1d167172ac9
Todd Thomas <noodlepopllc@gmail.com>
2026-09-15 08:13:32 -0400

    Changed quant

--- plan10/lib/mmh3.py
+++ plan10/lib/mmh3.py
@@ -477,7 +477,6 @@ def compose_video(prompt, images, audio, output='output.mp4', width=768, height=
     try:
         vram_limit = min(VRAM, 64)
         quant = "comfy_kitchen_int8_w8a8"
-        quant = "torchao_int4_w4a8"
         if vram_limit < 32:
             vram_config = {
                 "onload_dtype": torch.bfloat16,
