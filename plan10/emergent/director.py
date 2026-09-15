181dfbe9ebd4868b8ed1cf3030cb60ea565cbed3
Todd Thomas <noodlepopllc@gmail.com>
2026-09-14 21:21:31 -0400

    Added support for new wangp server

--- plan10/emergent/director.py
+++ plan10/emergent/director.py
@@ -17,7 +17,7 @@ class Director:
         ext = media_path.suffix.lower()
         media_type = "video" if ext in ['.mp4', '.avi', '.mov', '.mkv', '.webm'] else "image"
         
-        if video:
+        if "video" in media_type:
         # Stage 1: SmolVLM2 describes what it sees
             visual_description = AnalyzeMedia(str(media_path), f"Describe what you see in this video in detail with timestamps.", max_tokens=4096, temperature=0.4)
         else:
