47d2b822ad6a832c3d1c0edc2f3a41c30e7dfff7
Todd Thomas <noodlepopllc@gmail.com>
2026-09-15 09:52:14 -0400

    updated image to video for ltx

--- plan10/emergent/video_runner.py
+++ plan10/emergent/video_runner.py
@@ -453,7 +453,7 @@ def main():
         else:
             from plan10.emergent.ltx25_previewer import LTXPipeline
             converter = LTXPipeline()
-            converted = converter.run(pending_job['output_path'].replace('.mp4', '_script.txt', style='', use_descriptions=True)
+            converted = converter.run(pending_job['output_path'].replace('.mp4', '_script.txt'), style='', use_descriptions=True)
             print(converted)
             Path(output).write_text(f'RUNLENGTH (s):{converter.run_length}\n{converted}')
 
