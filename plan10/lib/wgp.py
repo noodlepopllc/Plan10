9e91f0d46ca79f2e87136e0d2b300734c91435ff
Todd Thomas <noodlepopllc@gmail.com>
2026-09-16 22:36:39 -0400

    fixed something

--- plan10/lib/wgp.py
+++ plan10/lib/wgp.py
@@ -375,14 +375,18 @@ def s2v_ltx(prompt='', media='', end_image='', audio='', text='', output='output
 
     last = ''
     dedupe_updates = set([])
-    while status := requests.get(f"http://127.0.0.1:8080/status/{job_id}").json()[-1] in ("pending","running"):
-        sleep(5)
-        update = requests.get(f"http://127.0.0.1:8080/updates/{job_id}").json()
-        if update:
-            update = update[0].strip()
-            if update not in dedupe_updates:
-                dedupe_updates.add(update)
-                print(update)
+    try:
+        while status := requests.get(f"http://127.0.0.1:8080/status/{job_id}").json():
+            if status[-1] in ("pending","running"):
+                sleep(5)
+                update = requests.get(f"http://127.0.0.1:8080/updates/{job_id}").json()
+                if update:
+                    update = update[0].strip()
+                    if update not in dedupe_updates:
+                        dedupe_updates.add(update)
+                        print(update)
+    except Exception as e:
+        print(e)
     print(requests.get(f"http://127.0.0.1:8080/status/{job_id}").json()[-2:])
 
 async def s2v_h3(prompt='', media='', end_image='', audio='', text='', output='output.mp4', 
