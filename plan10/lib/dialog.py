13d2ae3e9604e1087519bc3bff8029e01ae046b1
Todd Thomas <noodlepopllc@gmail.com>
2026-09-18 10:55:16 -0400

    Fixed backgrounds

--- plan10/lib/dialog.py
+++ plan10/lib/dialog.py
@@ -385,7 +385,9 @@ def main():
     if not args.ref_audio:
         DesignVoice(args.instruct, args.output, args.seed, args.long)
     elif args.transcribe and args.ref_audio:
-        output = ' '.join(str(transcribe(args.ref_audio, detailed=args.plus)))
+        output = str(transcribe(args.ref_audio, detailed=args.plus))
+        if not args.plus:
+            output = ' '.join(output)
         dur = -1
         if args.output.endswith('.txt'):
             if args.ref_audio.endswith('.mp4'):
