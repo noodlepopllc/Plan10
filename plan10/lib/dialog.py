a1e51963f8ad65205aab1b9a6dfd24128a2dbafe
Todd Thomas <noodlepopllc@gmail.com>
2026-09-18 10:49:50 -0400

    Fixed backgrounds

--- plan10/lib/dialog.py
+++ plan10/lib/dialog.py
@@ -18,7 +18,7 @@ class Transcription(object):
         text = ''
         text += "Detected language '%s' with probability %f\n" % (self.language, self..language_probability)
         for detail in details:
-            text += "[%.2fs -> %.2fs] %s\n" % detail['start'], detail['end'], detail['text']"
+            text += "[%.2fs -> %.2fs] %s\n" % detail['start'], detail['end'], detail['text']
         return text
 
 
