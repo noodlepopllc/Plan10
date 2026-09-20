f0c235afcee9d94c97f258b80e209d0fa5b31613
Todd Thomas <noodlepopllc@gmail.com>
2026-09-19 21:37:16 -0400

    Changed from omnivoice to AuK

--- plan10/lib/dialog.py
+++ plan10/lib/dialog.py
@@ -43,14 +43,14 @@ def patch_auk_yaml(yaml_path):
     # AuK-Flash uses text_encoder_path
     if "text_encoder_path" in model.get("text_encoder", {}):
         rel = model["text_encoder"]["text_encoder_path"]
-        tail = Path(rel).name
+        tail = pathlib.Path(rel).name
         print("ENCODER PATH: ",tail)
         model["text_encoder"]["text_encoder_path"] = str(CKPTS / tail)
 
     # AuK-Base uses model_path
     if "model_path" in model.get("text_encoder", {}):
         rel = model["text_encoder"]["model_path"]
-        tail = Path(rel).name
+        tail = pathlib.Path(rel).name
         print("ENCODER PATH: ",tail)
         model["text_encoder"]["model_path"] = str(CKPTS / tail)
     cfg["model"] = model
