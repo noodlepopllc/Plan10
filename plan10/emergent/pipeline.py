cbf0057cd1f513e306ac176583133054c7652293
Todd Thomas <noodlepopllc@gmail.com>
2026-09-20 10:51:38 -0400

    Changed from omnivoice to AuK

--- plan10/emergent/pipeline.py
+++ plan10/emergent/pipeline.py
@@ -194,8 +194,9 @@ class Pipeline:
 
         # FIRST BEAT: Store initial media and animate directly
         if not history:
-            current_state = f"{' and '.join([x for x in self.visual_ids])} is now visible in the scene, facing the camera in a frontal or 3/4 view."
-            current_media, current_bg = self.recreate_frame(current_media, current_bg, current_state, beat_count)
+            if LTX:
+                current_state = f"{' and '.join([x for x in self.visual_ids])} is now visible in the scene, facing the camera in a frontal or 3/4 view."
+                current_media, current_bg = self.recreate_frame(current_media, current_bg, current_state, beat_count)
             self.initial_media = str(current_media)
             print("🎬 First beat - animating initial scene...")
             
