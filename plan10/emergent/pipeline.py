f99e4c6c44119bf6bc7ed672a04880200241d526
Todd Thomas <noodlepopllc@gmail.com>
2026-09-20 10:28:46 -0400

    Changed from omnivoice to AuK

--- plan10/emergent/pipeline.py
+++ plan10/emergent/pipeline.py
@@ -194,7 +194,7 @@ class Pipeline:
 
         # FIRST BEAT: Store initial media and animate directly
         if not history:
-            current_media, current_bg = recreate(current_media, current_bg, current_state, beat_count)
+            current_media, current_bg = recreate_frame(current_media, current_bg, current_state, beat_count)
             self.initial_media = str(current_media)
             print("🎬 First beat - animating initial scene...")
             
