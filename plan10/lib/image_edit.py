4f30cb735072a7fb234b5b5d2c9d3dedff8c0b5c
Todd Thomas <noodlepopllc@gmail.com>
2026-09-21 22:14:15 -0400

    Added second audio pass

--- plan10/lib/image_edit.py
+++ plan10/lib/image_edit.py
@@ -239,7 +239,7 @@ class ImageEditQwen2(object):
             "onload_dtype": "disk",
             "onload_device": "disk",
             "preparing_dtype": torch.bfloat16,
-            "preparing_device": "cuda",
+            "preparing_device": "cpu",
             "computation_dtype": torch.bfloat16,
             "computation_device": "cuda"
             }
