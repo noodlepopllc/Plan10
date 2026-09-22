a3ba8a1150186bb4eb127b8f042146c2e808772c
Todd Thomas <noodlepopllc@gmail.com>
2026-09-22 08:00:55 -0400

    Added second audio pass

--- plan10/lib/director_mmh3.py
+++ plan10/lib/director_mmh3.py
@@ -614,7 +614,8 @@ def send(prompt, images, audio, output='output.mp4', width=768, height=448, dura
     args["embedded_guidance_scale"] = 1.5
     args['resolution'] = f'{width}x{height}'
     args['video_length'] = (((duration * 24) // 17) * 17) + 5
-    args["custom_settings"] = { "h3_mask_mode": "grouped_rows", "audio_refinement": "enabled" }
+    if steps >= 8:
+        args["custom_settings"] = { "h3_mask_mode": "grouped_rows", "audio_refinement": "enabled" }
     print(args)
     job_id = requests.post("http://127.0.0.1:8080/run", json=args).json()
     print(job_id)
