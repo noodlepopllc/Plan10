4b2c72ef341c7d57a03a2413013d9ed033b2264b
Todd Thomas <noodlepopllc@gmail.com>
2026-09-23 12:26:35 -0400

    Updated h3 to support loras

--- plan10/lib/image_gen.py
+++ plan10/lib/image_gen.py
@@ -126,6 +126,7 @@ class ImageGenKrea2(object):
             self.__enter__()
         image = self.pipe(
                 prompt=prompt,
+                negative_prompt="watermarks"
                 seed=seed,
                 height=height,
                 width=width,
